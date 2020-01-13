c7dcfd369dad98064bff6e4406e195d41ff45086
yeehaw241 <lazarou.phil@gmail.com>
2020-01-13 22:58:17 +0000

    Updated to catch Guzzle exception and display error message in page.

--- NOTES.md
+++ NOTES.md
@@ -15,4 +15,17 @@
 ## How To Test
 - On loading the page "Behind The Laughter - s11e22" is now the last episode. 
 ## Deployment steps
-- merge issue-1-sort-episode-order into master 
+- merge issue-1-sort-episode-order into master
+
+# [issue-2-handle-api-server-errors-when-retrieving-episode-data]
+
+## Features
+- Wrapped the guzzle clienyt request in a try/catch to handle exception event and display user friendly message
+## How To Test
+- Attempt refreshing the page several times.
+## Future Improvement
+- Install test suite such as PhpUnit
+- Move the episode loading code into it's own class
+- Throw the exception in a test to verify feature
+## Deployment steps
+- merge issue-2-handle-api-server-errors-when-retrieving-episode-data
--- public/index.php
+++ public/index.php
@@ -3,17 +3,23 @@ require_once '../vendor/autoload.php';
 
 //Load Twig templating environment
 $loader = new Twig_Loader_Filesystem('../templates/');
-$twig = new Twig_Environment($loader, ['debug' => true]);
+$twig   = new Twig_Environment($loader, ['debug' => true]);
+$error  = false;
+$data   = [];
 
-//Get the episodes from the API
-$client = new GuzzleHttp\Client();
-$res = $client->request('GET', 'http://3ev.org/dev-test-api/');
-$data = json_decode($res->getBody(), true);
-
-//Sort the episodes
-$seasons  = array_column($data,"season");
-$episodes = array_column($data,"episode");
-array_multisort($seasons, SORT_ASC, $episodes, SORT_ASC, $data);
+try{
+	//Get the episodes from the API
+	$client = new GuzzleHttp\Client();
+	$res    = $client->request('GET', 'http://3ev.org/dev-test-api/');
+	$data   = json_decode($res->getBody(), true);
+	
+	//Sort the episodes
+	$seasons  = array_column($data,"season");
+	$episodes = array_column($data,"episode");
+	array_multisort($seasons, SORT_ASC, $episodes, SORT_ASC, $data);	
+} catch (GuzzleHttp\Exception\ServerException $ex) {
+    $error = true;
+}
 
 //Render the template
-echo $twig->render('page.html', ["episodes" => $data]);
+echo $twig->render('page.html', ["episodes" => $data,"error" => $error]);
